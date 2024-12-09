1282921f2aa328860bc7a5f5243594411d5489d5
Rabi Bhagat <rabibhagat789@gmail.com>
2024-12-09 23:37:44 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,21 @@
+/*console.log("hello");
+let a = 10;
+let b =  10;
+console.log(a == b);
+console.log("lskhdjgkahd");
+// alert("welcome"); 
+*/
+
+/*let a = "go go ";
+let b = a;
+console.log(a);
+*/
+// let a = "ekoirfr" - "jjj";
+// console.log(a);
+// console.log(typeof a);
+// console.log(1%2);
+// 
+let a = 2;
+
+console.log(++a);
+
