3eec59038f8e45a921b8060eaf3a250e828ea6a7
Rabi Bhagat <rabibhagat789@gmail.com>
2024-12-13 23:33:34 +0530

    Add files via upload

---
+++ vaiable.js
@@ -0,0 +1,33 @@
+// for declaring the variable in js we use:-
+// 1. let
+// 2.var
+// 3. const
+
+let a = 5; //let is used for variable that may change
+console.log(a);
+// it gives  5  in output
+
+ a = 10;
+
+ console.log(a);
+//  it gives the 10 in output
+
+
+var b = 15; //var is used for variable that uless necessary for legacy
+console.log(b);
+// it gives 15 in output
+
+b = 20;
+console.log(b);
+// it gives 20 in output
+
+
+const c = 25; //for constant a value that won't be changed
+console.log(c);
+// it gives 25 output
+
+c = 30;
+console.log(c);
+// it gives error that assignment to a constant variable
+
+
