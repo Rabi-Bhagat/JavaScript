a3090a6936be0a5bfe5f49c19faf94c50df73342
Rabi Bhagat <rabibhagat789@gmail.com>
2024-12-14 23:56:45 +0530

    Add files via upload

---
+++ typeconversion.js
@@ -0,0 +1,14 @@
+let a = "i am on the way";
+let b = 5;
+c = a+b;
+console.log(c);
+console.log(typeof c);
+
+// str + num =str 
+
+// explict type conversion
+console.log("here is the explict conversion:-");
+
+a = "4";
+a = Number(a);
+console.log(typeof a);
