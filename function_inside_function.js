4d04192a1cde8494d3be053d32e9c8a6f6d86953
Rabi Bhagat <rabibhagat789@gmail.com>
2024-12-18 00:44:23 +0530

    Add files via upload

---
+++ function_inside_function.js
@@ -0,0 +1,17 @@
+ function fun() {
+    console.log("i am a busy mam");
+    function fun2(){
+        console.log(' i am function inside the function'); 
+        
+    }
+        console.log(fun2);
+        
+        
+
+
+       
+    }
+    fun();
+    
+    
+ 
