73ff391bc92ebb873a5abd6c907f8bd6bf7d881c
Rabi Bhagat <rabibhagat789@gmail.com>
2024-12-30 21:45:00 +0530

    Add files via upload

---
+++ proto.js
@@ -0,0 +1,13 @@
+var x = {
+    name:'rabi',
+};
+
+var y =Object.create(x)
+console.log(y);
+
+
+// var y = {
+//     lastname: ' Bhagat',
+// };
+// y.__proto__ = x;
+// console.log(y.name);
---
+++ prototype.js
@@ -0,0 +1,15 @@
+function Person(name,agae){
+    let Person = Object.create(obj)
+    person.name = name;
+    person.age = age;
+    return person
+}
+
+    // var Obj ={
+    // greet () {
+    // console. log( 'Hello $(this.namey)}
+    // let user - Person ("John" , 8)
+    // user. greet( )   
+    // console. log (user)
+    // console. log (Person. prototype)
+    // }
