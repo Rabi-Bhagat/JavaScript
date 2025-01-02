e9103610a66aa05f1254009699063b35cdbb8c75
Rabi Bhagat <rabibhagat789@gmail.com>
2025-01-02 07:47:24 +0000

    al about js

---
+++ basic/datatype_conversion.js
@@ -0,0 +1,25 @@
+// let a = 33;
+let a = "33asd";
+//  if we add like string in a then it gives the output of number
+// like a = 38u83u
+let b = "33";
+
+console.log(typeof a);
+console.log(typeof b);
+
+let valuenumber = Number(a);
+console.log(a);
+console.log(valuenumber);
+console.log(typeof valuenumber);
+
+//here is we can easily convert the 
+//number into string("3" => 3[number])
+// string int0 Nan[not a number]("3abd" => nan )
+//true => 1 
+//false => 0
+
+//boolean convert
+// 1 => true
+// 0 => false
+//" " => null
+//string => true
---
+++ basic/datatypes.js
@@ -0,0 +1,14 @@
+'use strict' // this is to treat all the js code as newer version
+let a = 5;
+let b = 'Rabi';
+const c = true;
+
+console.log(a);
+console.log(typeof a);
+console.log(b);
+console.log(typeof b);
+console.log(c);
+console.log(typeof c);
+
+console.log(typeof null); // null is the object
+console.log(typeof undefined); //it is the undefined
