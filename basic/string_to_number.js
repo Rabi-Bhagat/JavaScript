683816bd805532b2d6efdbc10bef42d7c7f00690
Rabi Bhagat <rabibhagat789@gmail.com>
2025-01-02 08:06:33 +0000

    all from js

---
+++ basic/string_to_number.js
@@ -0,0 +1,9 @@
+// let a = "rabi";
+// let b = " bhagat";
+// let c =  a + b;
+// console.log(c);
+
+// string or number adding
+ 
+console.log("1" + 4 + 4); // 144 output
+console.log(1 + 4 + "4"); // 54 putput
