9bed639e7d57674c80dd5a69019c008720d503b3
Rabi Bhagat <rabibhagat789@gmail.com>
2025-01-03 05:07:20 +0000

    all about js

---
+++ playlist/date_and-time.js
@@ -0,0 +1,21 @@
+// let date = new Date(); // date in always capital D
+// console.log(date);
+// // console.log(date.toString());
+// console.log(date.toLocaleString());
+// console.log(typeof date); //date is object
+
+// let suppose = new Date( 2006,3,7); //in js month is start from 0
+// let suppose = new Date( 2006,3,7,3,20,56);
+// console.log(suppose);
+// console.log(suppose.toLocaleString());
+
+// let timestamp = Date.now()
+// console.log(timestamp);
+// console.log(suppose.getTime()); //timestamp is not an object
+// console.log(Date.now());
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));  //in  millisecound
+
+let day = new  Date();
+console.log(day.getDay()); // day start from 0 in js
+console.log(`${day.getDay()} and the date is `);
