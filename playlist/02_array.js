16a450158f16cd1d519808caa7066cbfcf4bfbf0
Rabi Bhagat <rabibhagat789@gmail.com>
2025-01-05 17:19:06 +0000

    all about js

---
+++ playlist/02_array.js
@@ -0,0 +1,18 @@
+scl_frnd =['a','b','c','d','e'];
+clg_frnd = ['w','x','y','z'];
+// scl_frnd.push(clg_frnd);
+// console.log(scl_frnd);
+// console.log(clg_frnd);
+// console.log(scl_frnd[5][1]);
+
+// const concat = scl_frnd.concat(clg_frnd);
+// console.log(concat); // concat the two array in the same array.
+// console.log(...scl_frnd,...clg_frnd); //this is spread
+array = [1,2,3,4,5,6,7];
+console.log(Array.isArray("Rabi"))
+console.log(Array.from("Rabi"))
+console.log(Array.from({name:"rabi"})); //mazza hai
+
+
+console.log(array);
+
