c17c6c61d22a3f7a960330cf31f25ac3c5a9ef7f
Rabi Bhagat <rabibhagat789@gmail.com>
2025-01-09 08:19:09 +0000

    all about js

--- playlist/global_and_local_variable.js
+++ playlist/global_and_local_variable.js
@@ -10,12 +10,51 @@ let a = 333;
     let a = 30;
     var b = 22;
     const c = 33;
-    console.log(a);
+   //  console.log(a);
     
  }
 
 //  console.log(a); //we can't acess the value of let outside the scope
- console.log(b); //we can easily acess the value of var outside the scope so the var is not in used and it create a lot of problems further 
+//  console.log(b); //we can easily acess the value of var outside the scope so the var is not in used and it create a lot of problems further 
 //  console.log(c);
- console.log(a);
+//  console.log(a);
  
+ //nested function
+/*
+ function first(){
+   const myname = "Rabi";
+
+   function second (){
+      const lastname = "Bhagat";
+      console.log(myname);
+      
+   }
+
+   // console.log(lastname); //here is the error occur deu to we can't acess the lastname otuside the scope
+   
+   second();
+
+ }
+
+ first();
+ */
+/*
+ function aryan(){
+   const country = 'Nepal';
+
+   function man(){
+      const district = "  dhanusha";
+      console.log(country + district);
+      
+
+   }
+   man();
+
+ }
+ aryan();
+*/
+
+function adding(numm){
+   return numm +1
+}
+console.log(adding(2));
