b035199bd727893c7f533e4bf1004c3da950afcf
Rabi Bhagat <rabibhagat789@gmail.com>
2025-01-11 18:18:56 +0000

    all about js

--- playlist/contolflow_in_js.js
+++ playlist/contolflow_in_js.js
@@ -1,1 +1,43 @@
- 
+//if else
+const value = 50;
+if(value>20){
+    console.log("i am grater than 20");
+    
+}
+
+else{
+    console.log("i am smaller tham 20");
+    
+}
+
+//implicit scope (run the code in one line )
+const paisa = 10000;
+if(paisa >5000) console.log("your paisa is grater than 5k");
+
+//nested if else
+
+const taka = 1000;
+if(taka > 500){
+    console.log("your taka is grater than 500");
+    
+}
+else if(taka>750){
+    console.log("your taka is grater than 750");
+    
+}
+else{
+    console.log("your taka isless than 50");
+    
+}
+
+// switch
+const mean = 4;
+switch(mean >4) {
+    case 1:
+        console.log("i am  in 1 jan
+            ");
+            default()
+        
+}case 2:
+console.log(" i am from 2
+    ");
