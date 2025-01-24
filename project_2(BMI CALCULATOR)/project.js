7d88801dd492afa4fa57ec796364c2e4120b55bb
Rabi Bhagat <rabibhagat789@gmail.com>
2025-01-24 18:15:22 +0000

    all about bmi project

---
+++ project_2(BMI CALCULATOR)/project.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form');
+ // this use give empeety 
+form.addEventListener('submit',function(sum){
+    sum.preventDefault();
+
+
+const height = parseInt(document.querySelector('#height').value);
+const weight = parseInt(document.querySelector('#weight').value) 
+
+});
