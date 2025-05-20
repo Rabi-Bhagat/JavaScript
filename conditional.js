b2911ad42dc333e715e759feac82da5bbb06e249
Rabi-Bhagat <rabibhagat789@gmail.com>
2025-05-20 19:27:05 +0545

    revised

---
+++ conditional.js
@@ -0,0 +1,14 @@
+// for loop
+
+// var i = 1;
+for (let i = 0; i <= 5; i++) {
+    const element = [i];
+    
+    console.log(element);
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log("i am the best.");
+    
+    
+}
