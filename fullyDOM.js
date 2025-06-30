fc05ab6c6fc536fd875fb035b2c3f644536b5dae
Rabi Bhagat <rabibhagat789@gmail.com>
2025-06-30 08:28:50 +0545

    event handling

--- fullyDOM.js
+++ fullyDOM.js
@@ -1,1 +1,6 @@
-alert("You can do it ..Aabb roko~!!!!!!!!!!!")
+// alert("You can do it ..Aabb roko~!!!!!!!!!!!")
+// let rooko = document.querySelector("p");
+// console.log(rooko.setAttribute("class","newclass"));
+
+// let p = document.querySelector('p');
+// p.style.backgroundColor = "blue";
